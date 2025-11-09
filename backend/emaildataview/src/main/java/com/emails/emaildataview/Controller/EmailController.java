863c4ef8b6ec463c09455b62c0dc77766b60a9f0
wedsonpereira <pereirawedson1996@gmail.com>
2025-11-10 00:40:19 +0530

    updated the API request handler

--- backend/emaildataview/src/main/java/com/emails/emaildataview/Controller/EmailController.java
+++ backend/emaildataview/src/main/java/com/emails/emaildataview/Controller/EmailController.java
@@ -1,4 +1,50 @@
 package com.emails.emaildataview.Controller;
 
+import com.emails.emaildataview.DataSkeleton.Client;
+import com.emails.emaildataview.Dao.EmailDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/emails")
 public class EmailController {
+
+    @Autowired
+    private EmailDao emailDao;
+
+    @GetMapping("/")
+    public ResponseEntity<List<Client>> getAllClients() {
+        try {
+            List<Client> clients = emailDao.findAll();
+            return ResponseEntity.ok(clients);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+// give this API to the frontend of the thumbeja Publicity Website
+    @PostMapping("/contact")
+    public ResponseEntity<Client> submitContacts(@RequestBody Client client) {
+        try {
+            Client savedClient = emailDao.save(client);
+            return ResponseEntity.status(HttpStatus.CREATED).body(savedClient);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Client> getClientById(@PathVariable String id) {
+        try {
+            return emailDao.findById(id)
+                    .map(ResponseEntity::ok)
+                    .orElse(ResponseEntity.notFound().build());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
 }
--- backend/emaildataview/src/main/java/com/emails/emaildataview/Dao/EmailDao.java
+++ backend/emaildataview/src/main/java/com/emails/emaildataview/Dao/EmailDao.java
@@ -1,9 +1,8 @@
 package com.emails.emaildataview.Dao;
 
 import com.emails.emaildataview.DataSkeleton.Client;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface EmailDao extends CrudRepository<Long, Client> {
-
+public interface EmailDao extends MongoRepository<Client, String> {
 
 }
